daf87bf62e4ae68ef3d096a0fb017ec69eb028dc
kadalgurun1337 <givemee1337@gmail.com>
2023-03-28 14:56:17 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <h1>testtrtrtr</h1>
+    <img alt="Vue logo" class="logo" src="https://th.bing.com/th/id/OIP.ptnwtjfxbJ2xy1UvwySvpgHaE8?pid=ImgDet&rs=1" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="ini adalah masbrooo" />
+      <p>jshdjsds</p>
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+
